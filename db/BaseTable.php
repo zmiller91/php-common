27476ccc8ec2f5472a0a87edc08cdd93f37f600e
Zachary Miller <2.4.8.16.32.sixtyfour@gmail.com>
2017-04-17 21:41:06 -0600

    Debugging, Comments, and Filtering
        - Added debug information to sql errors
        - Added method docs
        - Created a createAndFilter method

--- db/BaseTable.php
+++ db/BaseTable.php
@@ -8,6 +8,11 @@ class BaseTable
      * @var Connection 
      */
     private $m_oConn;
+    
+    /**
+     *
+     * @var ZError 
+     */
     public $m_oError;
     
     public function __construct($conn) 
@@ -27,6 +32,10 @@ class BaseTable
         //If there's an error in the query then die
         if(!$result){
             $this->m_oError->add("Database Error.");
+            if(DEBUG) {
+                $this->m_oError->add(mysqli_error($this->m_oConn->get()));
+                $this->m_oError->add($strQuery);
+            }
         }
         
         //If there is a mysqli_result then return it
@@ -43,6 +52,13 @@ class BaseTable
         return $result[0]['id'];
     }
     
+    /**
+     * Turns a result set into a map with the given keys
+     * 
+     * @param type $result
+     * @param type $keys
+     * @return type
+     */
     protected function map($result, $keys) {
         $retval = array();
         foreach($result as $r) {
@@ -52,8 +68,33 @@ class BaseTable
         return $retval;
     }
     
+    /**
+     * Escapes input for SQL injection
+     * 
+     * @param type $string
+     * @return type
+     */
     protected function escape($string) {
         return $this->m_oConn->get()->real_escape_string($string);
+    }
+    
+    /**
+     * Creates an AND SQL filter. Nulls will not be added and WHERE will 
+     * not be prefixed. The keys of $filter are column names and the values of
+     * $filter are the column's criteria.
+     * 
+     * @param array $filter
+     * @return string
+     */
+    protected function createAndEqualsFilter($filter) {
+        $sqlFilter = array();
+        foreach($filter as $column => $value) {
+            if(isset($value)) {
+                array_push($sqlFilter, "$column = '$value'");
+            }
+        }
+      
+        return implode(" AND ", $sqlFilter);
     }
     
     private function createKey($keys, $object) {
